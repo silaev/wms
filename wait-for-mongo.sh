8c58b8b58f85479524432ce8a184d0192a1fe8e1
Silaev, Konstantin [External] <Konstantin.Silaev@externals.adidas.com>
2019-06-21 19:45:05 +0300

    Add transactions, switch to openjdk11

---
+++ wait-for-mongo.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+attempt=0
+while [[ ${attempt} -le 59 ]]; do
+    attempt=$(( $attempt + 1 ))
+    echo "Waiting for server to be up (attempt: $attempt)..."
+    result=$(docker logs mongo)
+    if grep -q 'waiting for connections on port 27017' <<< $result ; then
+      echo "Mongodb is up!"
+      break
+    fi
+    sleep 2
+done
