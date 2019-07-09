31c60212b32ec88043659b40ba2fd596af0ad409
Silaev, Konstantin [External] <Konstantin.Silaev@externals.adidas.com>
2019-07-09 13:10:56 +0300

    test

--- tools/docker_image_tag_exists.sh
+++ tools/docker_image_tag_exists.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 function docker_image_tag_exists() {
-    echo "image:&1, tag:$2"
+    #echo "image:&1, tag:$2"
     EXISTS=$(curl -s https://hub.docker.com/v2/repositories/$1/tags/?page_size=10000 | jq -r "[.results? | .[]? | .name == \"$2\"] | any")
-    test $EXISTS = true
+    test ${EXISTS} = true
 }
 
 if docker_image_tag_exists $1 $2; then
