0e3e32ff492b7f4b7b9bd6a022661c7d280fbf41
Silaev, Konstantin [External] <Konstantin.Silaev@externals.adidas.com>
2019-07-15 21:07:32 +0300

    Add pass

--- tools/docker_image_tag_exists.sh
+++ tools/docker_image_tag_exists.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 function docker_image_tag_exists() {
-    #echo "image:&1, tag:$2"
     EXISTS=$(curl -s https://hub.docker.com/v2/repositories/$1/tags/?page_size=10000 | jq -r "[.results? | .[]? | .name == \"$2\"] | any")
     test ${EXISTS} = true
 }
---
+++ tools/pass_init.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+curl -fsSL "https://github.com/docker/docker-credential-helpers/releases/download/v0.6.0/docker-credential-pass-v0.6.0-amd64.tar.gz" | tar xv
+sleep 5
+chmod +x docker-credential-pass
+cat >key <<EOF
+     %echo Generating a basic OpenPGP key
+     Key-Type: DSA
+     Key-Length: 1024
+     Subkey-Type: ELG-E
+     Subkey-Length: 1024
+     Name-Real: s256
+     Name-Comment: with stupid passphrase
+     Name-Email: s256@gmail.com
+     Expire-Date: 0
+     Passphrase: $1
+     # Do a commit here, so that we can later print "done" :-)
+     %commit
+     %echo done
+EOF
+
+gpg --batch --generate-key key; pass init $(gpg -k | awk 'NR==4')
